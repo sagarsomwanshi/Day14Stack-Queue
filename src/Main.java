a22be6df16d315e8dfd4cc269ba240a2fa43cd5b
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 15:55:21 +0530

    Added : Perform peek & pop operation till stack is empty

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
         stack.push(30);
         stack.push(56);
         System.out.println(stack);
+        int size = stack.size();
+
+    for(int i =0; i<size;i++) {
+    System.out.println("peek operation :"+stack.peek());
+        System.out.println("pop operation : "+stack.pop() );
+    }
+        System.out.println("Stack After performing peek & pop operation :"+stack);
+        System.out.println("Is Stack Empty : "+stack.empty());
+
     }
 }
