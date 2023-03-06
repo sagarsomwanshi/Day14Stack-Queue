025ac91b553bd227636d36927af66dbbc61aefe1
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 15:34:40 +0530

    Added : Stack Creation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Stack & Queue");
-
+        Stack<Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        System.out.println(stack);
     }
 }
